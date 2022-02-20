decde5ac2593f2afcbccbedafa0dcb35520a6be9
Adamo Maia <adamomaia10@gmail.com>
2022-02-20 10:56:57 -0300

    Criação do projeto

---
+++ src/main/java/com/supera/demo/SuperaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.supera.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supera/demo/SuperaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supera.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
