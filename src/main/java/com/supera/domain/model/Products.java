5c7a92ff18d0604626adb85490f27ee4ebd62913
Adamo Maia <adamomaia10@gmail.com>
2022-02-21 22:05:35 -0300

    Adição, Listagem e remoção de produtos

---
+++ src/main/java/com/supera/domain/model/Products.java
@@ -0,0 +1,17 @@
+package com.supera.domain.model;
+
+import java.util.List;
+
+public class Products {
+	private List<Product> product;
+
+	public List<Product> getProduct() {
+		return product;
+	}
+
+	public void setProduct(List<Product> product) {
+		this.product = product;
+	}
+	
+	
+}
--- src/main/resources/db/migration/V001__create-table-product.sql
+++ src/main/resources/db/migration/V001__create-table-product.sql
@@ -2,7 +2,7 @@ create table product(
 	id bigint not null auto_increment,
 	name varchar(60) not null,
 	price decimal(5,2) not null,
-	score tinyint not null,
+	score int not null,
 	image varchar(60),
 
 	primary key(id)
